4dd5abf2c690dd816e00eb290d712cf6f26347b3
Al Siam <alsiamworld@gmail.com>
2022-07-04 21:59:58 +0600

    added gallery section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,20 @@
 // NextJS & React imports
-import type { NextPage } from 'next'
-import Image from 'next/image'
+import type { NextPage } from "next";
+import Gallery from "../Components/Gallery";
 
 // Third Party imports
 
 // Domestic imports
-import { SEO } from '../Components/SEO'
+import { SEO } from "../Components/SEO";
 
 /** Home page */
 const Home: NextPage = () => {
   return (
     <div>
-      <SEO 
-        title="Indipix"
-        description="" 
-      />
+      <SEO title="Indipix" description="" />
+      <Gallery />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- pages/saif.tsx
+++
@@ -1,5 +0,0 @@
-function Saif() {
-  return <div>Hello From Saif</div>;
-}
-
-export default Saif;
