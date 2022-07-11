82fe0991909ffcf81c76fbf54f365d0b92fa1287
imtiazkun <alshahriar.me@gmail.com>
2022-07-11 20:01:45 +0600

    Changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,6 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
-  theme:
-  {
-    backgroundSize: {
-      'auto': 'auto',
-      'cover': 'cover',
-      'contain': 'contain',
-      '100%': '100% 100%',
-
-    }
-
-  },
+  theme:{},
   plugins: [],
 }
