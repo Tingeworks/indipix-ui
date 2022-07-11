d4df0f7c659848553e2516627611f23b1087c698
imtiazkun <alshahriar.me@gmail.com>
2022-07-11 19:39:02 +0600

    Rectified Document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,12 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document'
 
-class MyDocument extends Document {
-  render() {
+export default function Document() {
     return (
       <Html>
         <Head>
           {/* nunito font */}
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link
             href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;0,800;0,900;1,200;1,300;1,400;1,600;1,700&display=swap"
             rel="stylesheet"
@@ -15,7 +14,7 @@ class MyDocument extends Document {
 
           {/* inter font */}
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@200;300;500;600;700;800;900&family=Nunito:ital,wght@0,200;0,300;0,400;0,500;0,600;0,800;0,900;1,200;1,300;1,400;1,600;1,700&display=swap"
             rel="stylesheet"
@@ -33,7 +32,5 @@ class MyDocument extends Document {
         </body>
       </Html>
     );
-  }
 }
 
-export default MyDocument;
