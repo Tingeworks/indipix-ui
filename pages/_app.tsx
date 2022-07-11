58906fb8de4c63462e5bbb490f65146d149a141e
imtiazkun <alshahriar.me@gmail.com>
2022-07-11 21:42:36 +0600

    Updated _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,7 +15,6 @@
 
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import Layout from '../components/Layout/Layout'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
