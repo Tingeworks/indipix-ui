7df10432ed69c79e2b3d70ee34440cb0334de44c
imtiazkun <alshahriar.me@gmail.com>
2022-07-12 10:15:09 +0600

    Fixed Component Naming

--- Components/Form/SearchBox.tsx
+++ Components/Form/SearchBox.tsx
@@ -3,11 +3,11 @@ import React from "react";
 import { FaSearch } from "react-icons/fa";
 
 /** Textfield in the banner component */
-const textFieldWithDropdown: React.FC = () => (
+const SearchBox: React.FC = () => (
   <div className="w-full flex bg-white py-4 rounded-sm shadow-lg">
     <div className=" text-gray-600 px-4 flex items-center"><FaSearch /></div>
     <input placeholder="Search for images and galleries" type="text" className="focus:outline-none pl-1 flex-1" />
   </div>
 );
 
-export default textFieldWithDropdown;
+export default SearchBox;
