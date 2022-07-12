b7e4a6311d29fd7be04d13b15ae9fd548d4e16d2
imtiazkun <alshahriar.me@gmail.com>
2022-07-12 10:08:47 +0600

    Updated Banner

--- Components/Banner.tsx
+++ Components/Banner.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 
 // Domestic Imports
-import TextFieldWithDropdown from "./form/TextFieldWithDropdown";
+import textFieldWithDropdown from "./form/textFieldWithDropdown";
 
 const Banner = () => {
   return (
@@ -16,7 +16,7 @@ const Banner = () => {
       <div className="flex items-center  justify-center w-full h-full">
         <div className="flex-col font-bold w-4/5 lg:w-4/5 xl:w-2/5 mb-10">
           <h1 className="text-white text-3xl mb-2">Find the best captures in India from professional lenses</h1>
-          <TextFieldWithDropdown />
+          <textFieldWithDropdown />
         </div>
       </div>
     </div>
