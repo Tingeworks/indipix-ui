6d42a5542c319e42a48b6456246a0163ec4ea08c
imtiazkun <alshahriar.me@gmail.com>
2022-07-18 23:31:28 +0600

    Added Button component

--- Components/Form/Button.tsx
+++ Components/Form/Button.tsx
@@ -3,14 +3,27 @@ import React from "react";
 
 // Interface
 interface ButtonProps {
-    Label: string;
-    children: React.ReactNode;
+  icon?: React.ReactNode;
+  Label: string;
+  type: "submit" | "button" | "reset";
+  style: "Primary" | "Secondary" | "Danger" | "Warning";
+  className?: string;
 }
 
 /** Textfield in the banner component */
-const Button: React.FC <ButtonProps> =(props, { children})=> (
-  <button className="border-2 border-gray-200 text-gray-400 rounded-sm px-2 py-2">
-    {children} 
+const Button: React.FC<ButtonProps> = (props) => (
+  <button
+    type={props.type}
+    className={`
+      ${ props.style == "Primary" && " bg-red-700 text-white hover:bg-black "}
+      ${ props.style == "Secondary" && "bg-gray-200 text-gray-500 hover:bg-gray-300 "}
+      ${ props.style == "Danger" && "bg-red-500 text-gray-500 hover:bg-gray-300 "}
+      ${ props.style == "Warning" && " bg-orange-500 text-white hover:bg-black "} 
+      ${ props.className !== null && props.className}
+       px-2 py-2 rounded ${props.icon !== null ? "flex items-center justify-center" : ""
+    }`}
+  >
+    {props.icon !== null ? <span className="pr-2">{props.icon}</span> : ""}
     <span>{props.Label}</span>
   </button>
 );
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,8 @@ import type { NextPage } from "next";
 import SEO from "../Components/Misc/SEO";
 import Layout from "../Components/Layout/Layout";
 import Banner from "../Components/Banner";
+import Button from "../Components/Form/Button";
+import { FaSearch } from "react-icons/fa";
 
 /** Home page */
 const Home: NextPage = () => {
@@ -14,6 +16,13 @@ const Home: NextPage = () => {
     <Layout isLoggedIn={false}>
       <SEO title="Indipix" description="" keywords="" />
       <Banner />
+      <Button style="Primary" Label="Submit" type="submit" />
+      <br />
+      <Button style="Primary"  icon={<FaSearch />} Label="Search" type="button" />
+      <br />
+      <Button style="Secondary" Label="Search" type="button" />
+      <br />
+      <Button style="Warning" Label="Search" type="button" className=" mx-6 my-3  text-2xl px-10" />
     </Layout>
   );
 };
