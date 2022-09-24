291f50e9bc8c3a0b19f7cefc3f97df6efa948513
imtiazkun <alshahriar.me@gmail.com>
2022-09-24 18:40:56 +0600

    Added checkout page

--- pages/imgs/[img].tsx
+++ pages/imgs/[img].tsx
@@ -10,6 +10,7 @@ import SEO from "../../Components/Misc/SEO";
 import Gallery from "../../Components/Gallery/Gallery";
 import SearchBox from "../../Components/Form/SearchBox";
 import { FaSearchPlus } from "react-icons/fa";
+import Link from "next/link";
 
 /** Image page */
 const Image: NextPage = () => {
@@ -25,19 +26,52 @@ const Image: NextPage = () => {
       <SEO title={`Indipix | ${img}`} description="" keywords="" />
       <div className="py-5" style={{ backgroundImage: "url(/banner.png)" }}>
         <div className="container mx-auto px-5 lg:px-20">
-        <SearchBox className="bg-white" />
+          <SearchBox className="bg-white" />
         </div>
       </div>
 
-      <div className="flex gap-10 container mx-auto px-5 lg:px-20 my-10 items-center">
-        <div className="flex-1 relative cursor-pointer">
-          <img src="https://source.unsplash.com/random/600x500" alt="" />
-          <FaSearchPlus className="text-white text-2xl drop-shadow-lg absolute bottom-0 right-0 m-5" />
+      <div className="flex flex-col lg:flex-row gap-10 container mx-auto px-5 lg:px-20 my-10 items-center">
+        <div className="flex-1 w-full">
+          <div className="relative cursor-pointer">
+            <img
+              className="w-full"
+              src="https://source.unsplash.com/random/600x500"
+              alt=""
+            />
+            <FaSearchPlus className="text-white text-2xl drop-shadow-lg absolute bottom-0 right-0 m-5" />
+          </div>
+          <div className="mt-5 flex gap-5">
+            <button className="rounded-full text-xs font-bold border-[#F87C52] border-4 px-8 py-0.5 uppercase">
+              Save
+            </button>
+            <button className="rounded-full text-xs font-bold  border-[#F87C52] border-4 px-8 py-0.5 uppercase">
+              Try
+            </button>
+            <button className="rounded-full text-xs font-bold border-[#F87C52] border-4 px-8 py-0.5 uppercase">
+              Share
+            </button>
+            <Link href="/checkout/subscription">
+              <button className="rounded-full hover:bg-black hover:border-black text-xs font-bold border-[#F87C52] text-white bg-[#F87C52] border-4 px-8 py-0.5 uppercase">
+                Download
+              </button>
+            </Link>
+          </div>
         </div>
-        <div className="flex-1">
-          <h1 className="text-2xl font-bold">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui, saepe.</h1>
+        <div className="flex-1 lg:-mt-10">
+          <h1 className="text-2xl font-bold">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui,
+            saepe.
+          </h1>
           <p className="text-slate-400 text-xs my-5">PHOTO ID- {img}</p>
-          <p className="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis dolorem delectus dignissimos sit cupiditate quisquam repudiandae alias. Aliquid quod tempora vitae voluptas quos veniam excepturi optio fugiat illo assumenda nobis enim deserunt, placeat, rerum, voluptatum provident quasi! Repellendus aliquam quibusdam quasi esse, quod labore deleniti minima, error quia reprehenderit provident?</p>
+          <p className="text-sm">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
+            dolorem delectus dignissimos sit cupiditate quisquam repudiandae
+            alias. Aliquid quod tempora vitae voluptas quos veniam excepturi
+            optio fugiat illo assumenda nobis enim deserunt, placeat, rerum,
+            voluptatum provident quasi! Repellendus aliquam quibusdam quasi
+            esse, quod labore deleniti minima, error quia reprehenderit
+            provident?
+          </p>
           <p className="font-bold mt-2 text-lg">Rs. 2000 - Rs. 5000</p>
         </div>
       </div>
