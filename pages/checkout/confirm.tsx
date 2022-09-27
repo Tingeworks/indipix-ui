97dcfcb803ea82aa1ed1d5bc8d6d92be611dab83
imtiazkun <alshahriar.me@gmail.com>
2022-09-27 14:47:12 +0600

    Checkout page fix

--- pages/checkout/confirm.tsx
+++ pages/checkout/confirm.tsx
@@ -4,7 +4,7 @@ import SEO from "../../Components/Misc/SEO";
 import { useState } from "react";
 import Button from "../../Components/Form/Button";
 
-export default function confirm(props: any) {
+export default function Confirm(props: any) {
   const [imageformat, setImageFormat] = useState(0);
   const [subscription, setSubscription] = useState(0);
 
--- pages/checkout/select.tsx
+++ pages/checkout/select.tsx
@@ -4,7 +4,7 @@ import SEO from "../../Components/Misc/SEO";
 import { useState } from "react";
 import Button from "../../Components/Form/Button";
 
-export default function select(props: any) {
+export default function Select(props: any) {
   const [imageformat, setImageFormat] = useState(0);
   const [subscription, setSubscription] = useState(0);
 
