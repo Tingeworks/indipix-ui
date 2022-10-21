65b3a64ef49b2bcbb67d80b61b3fa2ca35339179
imtiazkun <alshahriar.me@gmail.com>
2022-10-21 12:12:17 +0600

    added pricing page

---
+++ Components/PricingCard/PricingCard.tsx
@@ -0,0 +1,26 @@
+import { Button } from "@nextui-org/react";
+
+export default function PricingCard(props: any) {
+  return <div className={`bg-white`}>
+    {props.favourite ? <div className="h-8 flex items-center justify-center bg-yellow-400 text-xs font-bold uppercase">recommended</div>: <div className="h-8
+    "></div>}
+   <div className="px-5 py-2">
+   <h3 className="fonts-bold text-lg mt-5">{props.title}</h3>
+    <div className="text-sm">{props.description}</div>
+
+    <p className="mt-10 text-3xl font-bold">{props.limit}</p>
+    <p>Downloads</p>
+    <p className="text-xl font-bold">{props.price} INR/<small>month</small></p>
+
+    <button className="px-5 py-2 border-2 border-black rounded-full mt-5 hover:bg-black hover:text-white">Subscribe now</button>
+
+
+    <ul className="list-disc list-inside p-5 mt-5">
+        <li className=" text-xs">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</li>
+        <li className=" text-xs">Lorem ipsum dolor sit amet.</li>
+        <li className=" text-xs">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum!</li>
+        <li className=" text-xs">Lorem ipsum dolor sit amet.</li>
+    </ul>
+   </div>
+  </div>;
+}
--- pages/price.tsx
+++ pages/price.tsx
@@ -2,12 +2,51 @@ import Layout from "../Components/Layout/Layout";
 import nookies from "nookies";
 import CONFIG from "../CONFIG";
 import SEO from "../Components/Misc/SEO";
+import PricingCard from "../Components/PricingCard/PricingCard";
 
 export default function Price({ loggedIn }: any) {
   return (
     <Layout isLoggedIn={loggedIn}>
       <SEO title="Indipix" description="" keywords="" />
-      <div className="container">Hello</div>
+      <div className="container bg-[#EA6940] mx-auto px-5 lg:px-20">
+        <div className="flex items-end justify-center overflow-x-hidden pt-10">
+          {[2, 0.5, 3, 0.5, 2].map((item) => (
+            <img
+              className={`m-0 ${item == 0.5 && `scale-75`} ${
+                item == 2 && `scale-90`
+              } ${item == 3 && `scale-100`}`}
+              key={item}
+              src={`https://source.unsplash.com/random/300x400/?sig=${item}`}
+            />
+          ))}
+        </div>
+      </div>
+
+      {/* <div className="-mt-20" style={{ minHeight: "40vh" }}> */}
+      <div className="h-full text-center relative -mt-56 z-30 ">
+        <div
+          className=" pt-48"
+          style={{
+            backgroundSize: "cover",
+            backgroundImage: "url(/blurbackground.png)",
+          }}
+        ></div>
+        <div className="bg-[#AB1C1C]">
+          <h1 className="w-4/6 inline-block uppercase text-5xl font-bold text-white z-10 drop-shadow-2xl backdrop-blur-lg px-10">
+            GET THE PERFECT DEAL ON IMAGES FOR YOU AND YOUR TEAM
+          </h1>
+          <p className="text-[#FFFFFF] text-xl">
+            Gain access to the biggest stock library of India
+          </p>
+        </div>
+
+        <div className="p-10  bg-[#AB1C1C] grid grid-cols-4 gap-10">
+          {[{id:1, name: "team", price: 3000, limit: 10}, {id:2, name: "Enterprise", price: 20000, limit: "Custom"}, {id:3, name: "Personal", price: 1000, limit: 5}, {id:4, name: "Studio", price: 5000, limit: 25}].map((item) => (
+            <PricingCard favourite={item.id == 1} key={item.id} limit={item.limit} title={item.name} description={"Gain access to the biggest stock library of India"} price={item.price} />
+          ))}
+        </div>
+      </div>
+      {/* </di1v> */}
     </Layout>
   );
 }
